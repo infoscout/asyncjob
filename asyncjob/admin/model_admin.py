8fe9ccc79704f71b516f29922d979c52a8663780
Carson Gregory <carson.gregory@gmail.com>
2014-02-19 14:38:24 -0800

    Clarifying changelist filesize unit labels.

--- asyncjob/admin/model_admin.py
+++ asyncjob/admin/model_admin.py
@@ -55,8 +55,8 @@ class AsyncJobAdmin(ModelAdmin):
 
 
 def sizeof_fmt(num):
-    for x in ['bytes','KB','MB','GB']:
+    for x in ['B','kB','mB','gB']:
         if num < 1024.0:
-            return "%3.1f%s" % (num, x)
+            return "%3.1f %s" % (num, x)
         num /= 1024.0
-    return "%3.1f%s" % (num, 'TB')
+    return "%3.1f%s" % (num, 'tB')
